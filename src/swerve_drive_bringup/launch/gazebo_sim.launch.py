21e38d948fa7e48ca3b305c6f8aa2e0c559c58a2
lazytatzv <yano.tatsuki439@mail.kyutech.jp>
2026-09-21 07:56:25 +0900

    fix(bringup): add ExecuteProcess import to gazebo_sim.launch.py

--- src/swerve_drive_bringup/launch/gazebo_sim.launch.py
+++ src/swerve_drive_bringup/launch/gazebo_sim.launch.py
@@ -2,7 +2,7 @@ import os
 from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
 from launch.conditions import IfCondition, UnlessCondition
-from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription, RegisterEventHandler
+from launch.actions import DeclareLaunchArgument, ExecuteProcess, IncludeLaunchDescription, RegisterEventHandler
 from launch.event_handlers import OnProcessExit
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch.substitutions import Command, FindExecutable, LaunchConfiguration, PathJoinSubstitution, PythonExpression
